362a50836dd41de452496ed1590b3848e7732b09
SHAMMAH <shamzyjayy@gmail.com>
2024-03-28 19:25:06 +0000

    correct

--- app/models.py
+++ app/models.py
@@ -4,16 +4,16 @@ from sqlalchemy import Column, Integer, ForeignKey, String, Boolean, DateTime
 import datetime
 
 
-class USER(Base):
-    __tablename__ = "users"
+# class USER(Base):
+#     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True)
-    username = Column(String, unique=True)
-    firstname = Column(String)
-    lastname = Column(String)
+#     id = Column(Integer, primary_key=True, index=True)
+#     email = Column(String, unique=True)
+#     username = Column(String, unique=True)
+#     firstname = Column(String)
+#     lastname = Column(String)
 
-    urls = relationship("URL", back_populates="user")
+#     urls = relationship("URL", back_populates="user")
 
 
 class URL(Base):
@@ -23,8 +23,8 @@ class URL(Base):
     original_url = Column(String, nullable=False)
     shortened_url = Column(String, nullable=False, unique=True)
     creation_timestamp = Column(DateTime, default=datetime.datetime.utcnow)
-    creator_id = Column(Integer, ForeignKey("users.id"))
+    # creator_id = Column(Integer, ForeignKey()
     click_count = Column(Integer, default=0)
     expiration_timestamp = Column(DateTime)
 
-    user = relationship("USER", back_populates="urls")
+    # user = relationship("USER", back_populates="urls")
